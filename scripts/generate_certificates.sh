6920dd7ad256cbd31140ffab6ec910dacfbe1922
Ruben Gonzalez <rg@ht11.org>
2022-01-26 10:50:45 +0100

    Add script to generate certificates

---
+++ scripts/generate_certificates.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+KEMTLS_FOLDER=kemtls-reproducible
+if [ ! -d $KEMTLS_FOLDER ]; then
+    echo "No '$KEMTLS_FOLDER' folder. Are you in the experiments root dir?"
+    exit 1
+fi
+
+cd $KEMTLS_FOLDER
+echo "Running certificate builder in Docker."
+docker-compose run builder bash /mnt/scripts/gen_key.sh
