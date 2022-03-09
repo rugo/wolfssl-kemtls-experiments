d87c796135fe22c3da5f16e473f526501dde734d
Ruben Gonzalez <rg@ht11.org>
2022-03-09 16:11:00 +0100

    Fix scripts to actually use test case number.

--- scripts/build_header.py
+++ scripts/build_header.py
@@ -50,7 +50,7 @@ def fill_template(template_path, **kwargs):
 
     for k in kwargs:
         content = content.replace(f"${k}$", str(kwargs[k]))
-    
+
     return content
 
 
@@ -64,33 +64,36 @@ def main():
     if len(sys.argv) < 5:
         sys.stderr.write("Not enough arguments submitted. Do a: {sys.argv[0]} EPH_KEX_ALG CERT_ROOT_SIG_ALG CERT_KEM_ALG TESTCASE_NUMBER")
         sys.exit(1)
-    
+
     for path in PATHS_TO_CHECK:
         check_path_exists_crash(path)
-    
+
     eph_kex_alg = sys.argv[1]
     cert_sig_alg = sys.argv[2]
     cert_kem_alg = sys.argv[3]
-    testcase_num = int(1)
+    testcase_num = int(sys.argv[4])
 
     testcase_path = os.path.join(CERTIFICATE_PATH, f"{cert_sig_alg}_{cert_kem_alg}_{testcase_num:04d}_ca.crt")
-    
+
+    print("Using", testcase_path)
+
     check_path_exists_crash(testcase_path)
-    
+
     ca_cert_len, ca_cert_hex = cert_to_hex_bytes(testcase_path)
 
     if os.path.isfile(TARGET_HEADER_PATH):
         print(f"Path {TARGET_HEADER_PATH} already exists! Will overwrite!")
-    
+
     content = fill_template(
-            TEMPLATE_PATH, eph_kex_alg=eph_kex_alg, 
-            cert_sig_alg=cert_sig_alg, cert_kem_alg=cert_kem_alg, 
+            TEMPLATE_PATH, eph_kex_alg=eph_kex_alg,
+            cert_sig_alg=cert_sig_alg, cert_kem_alg=cert_kem_alg,
             ca_cert_len=ca_cert_len, ca_cert_hex=ca_cert_hex
     )
+    # print(content)
 
     with open(TARGET_HEADER_PATH, "w") as f:
         f.write(content)
-    
+
 
 if __name__ == '__main__':
     main()
