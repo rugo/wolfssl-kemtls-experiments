3c9ed68032fa007fede06e207fd8c76b24c56f29
Ruben Gonzalez <rg@ht11.org>
2022-03-22 12:57:31 +0100

    Change header inlcude location

--- scripts/templates/kemtls_ca.h
+++ scripts/templates/kemtls_ca.h
@@ -1,6 +1,8 @@
 #ifndef WOLFSSL_KEMTLS_CA_H
 #define WOLFSSL_KEMTLS_CA_H
 
+#include "kemtlsexperiments.h"
+
 #if defined(KEMTLS_CERT_ROOT_SIG_$cert_sig_alg$) && defined(KEMTLS_CERT_KEM_$cert_kem_alg$)
 const char ca_cert[] = {
 $ca_cert_hex$
